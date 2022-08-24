effbba8b44059f800454d4a0755cd2c18f2153a3
WongYAQi <wongyaqi@greaconsulting.com>
2022-08-24 18:57:01 +0800

    测试

--- src/commands/stop.ts
+++ src/commands/stop.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { getUserName } from "./enterUserName";
 import * as vscode from 'vscode'
+import _const from "../const";
 
 export default function stop () {
     // 接口返回时还没有触发 pm2 的回调，所以进入 loading 状态
     vscode.commands.executeCommand('setContext', 'backendHelper.status', 'loading');
-    axios.post('/stop/' + getUserName())
+    axios.post('http://127.0.0.1:3000/stop/' + getUserName()).then(() => {
+        vscode.commands.executeCommand('setContext', 'backendHelper.status', 'stopped');
+        vscode.commands.executeCommand(_const.COMMANDS.BACKENDREFRESH)
+    })
 }
