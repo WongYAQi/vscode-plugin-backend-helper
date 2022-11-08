03ffecbcf1edc9d01e1ad1db80b1ba93af2be384
WongYAQi <wongyaqi@greaconsulting.com>
2022-11-08 18:05:19 +0800

    dev 0.2.0 初始化

--- node/execute.backend.js
+++
@@ -1,5 +0,0 @@
-const { exec } = require("child_process");
-
-let child = exec('java -jar logwire-backend-starter.jar', { cwd: './logwire-backend/build-output/backend' })
-child.stdout.on('data', data => console.log(data))
-child.stderr.on('data', data => console.log(data))
