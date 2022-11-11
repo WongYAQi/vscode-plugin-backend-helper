c82e2761a5d79689e9884187f4c5d636ee432874
wangyaoqin <39297897+WongYAQi@users.noreply.github.com>
2022-11-11 18:11:54 +0800

    测试

--- src/platform/node/docker.ts
+++ src/platform/node/docker.ts
@@ -6,16 +6,16 @@
  * * [ ] 在容器内操作命令， Docker API.exec
  */
 
-import axios, { AxiosResponse } from 'axios'
 import Dockerode from 'dockerode'
 const os = require('os')
 class Docker {
-  request<T = any>(url: string): Promise<AxiosResponse<T>> {
-    throw new Error('request 方法需要被继承重载')
+  host = 'host.docker.internal'
+  docker: Dockerode
+  constructor () {
+    this.docker = new Dockerode({ host: this.host, port: 2375 })
   }
   getContainers () {
-    let docker1 = new Dockerode({ host: 'host.docker.internal', port: 2375 })
-    return docker1.listContainers()
+    return this.docker.listContainers()
   }
   createContainer () {
 
@@ -25,31 +25,20 @@ class Docker {
   }
 }
 
-class WindowDocker extends Docker {
-  override request<T = any>(url: string): Promise<AxiosResponse<T>> {
-    return axios('http://192.168.0.190:2375' + url)
-  }
-  override getContainers() {
-    let docker1 = new Dockerode({ host: '192.168.0.190', port: 2375 })
-    return docker1.listContainers()
-  }
+class DevDocker extends Docker {
 }
 
-class LinuxDocker extends Docker {
-  override request<T = any>(url: string): Promise<AxiosResponse<T>> {
-    return axios('http://192.168.0.190:2375' + url)
-  }
-}
 class ProductionDocker extends Docker {
-
+  host = '192.168.0.4'
+  constructor () {
+    super()
+  }
 }
 
 export function createDockerFactory () {
-  if (os.platform() === 'win32') {
-    return new WindowDocker()
-  } else if (process.env['DOCKER_ENV'] === 'production') {
+  if (process.env['DOCKER_ENV'] === 'production') {
     return new ProductionDocker()
   } else {
-    return new LinuxDocker()
+    return new DevDocker()
   }
 }
